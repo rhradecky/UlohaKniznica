35908ec40ae876e0fad877e469a3c6a84e50de14
hradecky <rhradecky@gmail.com>
2024-01-22 09:47:16 +0100

    notebook1

--- main.py
+++ main.py
@@ -4,5 +4,18 @@ class Kniha:
         self.isbn = isbn
         self.dostupna = True
 
- ####
+    def vypozicaj(self):
+        self.dostupna = False
+
+class Kniznica:
+    def __init__(self):
+        self.zoznam_knih = []
+
+    def pridaj_knihu(self, kniha):
+        self.zoznam_knih.append(kniha)
+
+    def vypozicaj_knihu(self, isbn_knihy):
+        for kniha in self.zoznam_knih:
+            if (kniha.isbn == isbn_knihy):
+                kniha.vypozicaj()
 
