7c085384cb1814c0a463dc602204101d4a0d395d
hradecky <rhradecky@gmail.com>
2024-01-22 10:07:46 +0100

    notebook1

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 class Kniha:
-    def __init__(self, nazov, isbn):
+    def __init__(self, nazov, autor, isbn, rok):
         self.nazov = nazov
+        self.nazov = autor
         self.isbn = isbn
+        self.rok = rok
         self.dostupna = True
 
     def vypozicaj(self):
@@ -18,4 +20,23 @@ class Kniznica:
         for kniha in self.zoznam_knih:
             if (kniha.isbn == isbn_knihy):
                 kniha.vypozicaj()
+
+
+
+
+kniha1 = Kniha("Harry Potter","Rowlingova",  1, 2010)
+kniha2 = Kniha("Pan Prstenov", "Tolkien", 10, 2003)
+kniha3 = Kniha("Vkladna Knizka", "Slovenska Sporitelna", 11, 2000)
+
+
+
+
+kniznica = Kniznica()
+kniznica.pridaj_knihu(kniha1)
+kniznica.pridaj_knihu(kniha2)
+
+
+
+print(kniha1)
+print(kniha2)
 
